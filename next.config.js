6bd0a69be70d0597f314740f35be164a04ea66c7
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-30 16:29:09 +0700

    🎉

--- next.config.js
+++ next.config.js
@@ -1,25 +1,8 @@
+const { createSecureHeaders } = require("next-secure-headers");
+
 module.exports = {
   async headers() {
-    return [
-      {
-        source: "/about",
-        headers: [
-          {
-            key: "X-About-Custom-Header",
-            value: "about_header_value",
-          },
-        ],
-      },
-      {
-        source: "/news/:id",
-        headers: [
-          {
-            key: "X-News-Custom-Header",
-            value: "news_header_value",
-          },
-        ],
-      },
-    ];
+    return [{ source: "/(.*)", headers: createSecureHeaders() }];
   },
   images: {
     domains: ["randomuser.me"],
