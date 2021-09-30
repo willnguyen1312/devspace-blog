6b01d13d091038810bee099eb4d4c0013324f360
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-30 15:34:10 +0700

    ^o^

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,10 +1,11 @@
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import marked from 'marked'
-import Link from 'next/link'
-import Layout from '@/components/Layout'
-import CategoryLabel from '@/components/CategoryLabel'
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import marked from "marked";
+import Link from "next/link";
+import Image from "next/image";
+import Layout from "@/components/Layout";
+import CategoryLabel from "@/components/CategoryLabel";
 
 export default function PostPage({
   frontmatter: { title, category, date, cover_image, author, author_image },
@@ -13,61 +14,61 @@ export default function PostPage({
 }) {
   return (
     <Layout title={title}>
-      <Link href='/blog'>Go Back</Link>
-      <div className='w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6'>
-        <div className='flex justify-between items-center mt-4'>
-          <h1 className='text-5xl mb-7'>{title}</h1>
+      <Link href="/blog">Go Back</Link>
+      <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
+        <div className="flex justify-between items-center mt-4">
+          <h1 className="text-5xl mb-7">{title}</h1>
           <CategoryLabel>{category}</CategoryLabel>
         </div>
-        <img src={cover_image} alt='' className='w-full rounded' />
+        <Image src={cover_image} alt="" className="w-full rounded" />
 
-        <div className='flex justify-between items-center bg-gray-100 p-2 my-8'>
-          <div className='flex items-center'>
-            <img
+        <div className="flex justify-between items-center bg-gray-100 p-2 my-8">
+          <div className="flex items-center">
+            <Image
               src={author_image}
-              alt=''
-              className='mx-4 w-10 h-10 object-cover rounded-full hidden sm:block'
+              alt=""
+              className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
             />
             <h4>{author}</h4>
           </div>
-          <div className='mr-4'>{date}</div>
+          <div className="mr-4">{date}</div>
         </div>
 
-        <div className='blog-text mt-2'>
+        <div className="blog-text mt-2">
           <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
         </div>
       </div>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticPaths() {
-  const files = fs.readdirSync(path.join('posts'))
+  const files = fs.readdirSync(path.join("posts"));
 
   const paths = files.map((filename) => ({
     params: {
-      slug: filename.replace('.md', ''),
+      slug: filename.replace(".md", ""),
     },
-  }))
+  }));
 
   return {
     paths,
     fallback: false,
-  }
+  };
 }
 
 export async function getStaticProps({ params: { slug } }) {
   const markdownWithMeta = fs.readFileSync(
-    path.join('posts', slug + '.md'),
-    'utf-8'
-  )
+    path.join("posts", slug + ".md"),
+    "utf-8"
+  );
 
-  const { data: frontmatter, content } = matter(markdownWithMeta)
+  const { data: frontmatter, content } = matter(markdownWithMeta);
   return {
     props: {
       frontmatter,
       content,
       slug,
     },
-  }
+  };
 }
